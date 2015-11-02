1cda03e7f982cf4b40ad7ba8f19a0d0117a4e8d9
unknown <rellingson@CARPMAC-13.fs.elon.edu>
2015-11-02 13:29:40 -0500

    changed flow of app

--- ElonParking/app/src/main/java/edu/elon/cs/elonparking/ChoosePassActivity.java
+++ ElonParking/app/src/main/java/edu/elon/cs/elonparking/ChoosePassActivity.java
@@ -9,11 +9,15 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 
 
 public class ChoosePassActivity extends Activity {
@@ -21,12 +25,15 @@ public class ChoosePassActivity extends Activity {
     private final String FILENAME = "save.txt";
     Spinner passes;
     String pass = "";
+    Spinner buildings;
+    String building;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_pass);
         passes = (Spinner)findViewById(R.id.passSelect);
+        buildings = (Spinner) findViewById(R.id.buildingSelect);
         try {
             getPersistentData();
         } catch (IOException e) {
@@ -41,6 +48,7 @@ public class ChoosePassActivity extends Activity {
 
     public void onSelectPass(View view) {
         pass = passes.getSelectedItem().toString();
+        building = buildings.getSelectedItem().toString();
         nextPage();
     }
 
@@ -51,6 +59,7 @@ public class ChoosePassActivity extends Activity {
             InputStream in = context.openFileInput(FILENAME);
             reader = new BufferedReader(new InputStreamReader(in));
             pass = reader.readLine();
+            System.out.print(pass);
         } finally {
             if (reader != null) {
                 reader.close();
@@ -58,10 +67,37 @@ public class ChoosePassActivity extends Activity {
         }
     }
 
+    private void putPersistentData() throws IOException {
+        Context context = getBaseContext();
+        Writer writer = null;
+        try {
+            OutputStream out = context.openFileOutput(FILENAME, Context.MODE_PRIVATE);
+            writer = new OutputStreamWriter(out);
+            System.out.print(pass);
+            writer.write(pass);
+        } finally {
+            if (writer != null) {
+                writer.close();
+            }
+        }
+    }
+
     private void nextPage() {
-        Intent intent = new Intent(getApplicationContext(), SavePassActivity.class);
-        intent.putExtra("pass", pass);
-        startActivity(intent);
+        if(pass.equals("Select Pass")) {
+            Toast.makeText(getApplicationContext(), "Please select a pass", Toast.LENGTH_SHORT).show();
+        } else if(building.equals("Select Building")) {
+            Toast.makeText(getApplicationContext(), "Please select a building", Toast.LENGTH_SHORT).show();
+        } else {
+            try {
+                putPersistentData();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Intent intent = new Intent(getApplicationContext(), ParkingActivity.class);
+            intent.putExtra("pass", pass);
+            intent.putExtra("building", building);
+            startActivity(intent);
+        }
     }
 
 
